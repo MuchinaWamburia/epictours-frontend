53c45588eba22d2a3a171bdfad4daa045d414aa1
Francisca <117682018+Francieorwa@users.noreply.github.com>
2023-03-31 19:55:40 +0300

    updated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -50,65 +50,51 @@ const ContactUs = () => {
         <div className="container p-10 gap-20">
           <div className="row">
             <div className="col-md-6">
-            <div className="text-black mb-10 bg-white p-10 rounded-lg shadow-lg">
-  <h2 className="text-3xl font-bold mb-8 text-center">
-    Contact Us
-  </h2>
-  <p className="text-lg mb-6 text-center">
-    We would love to hear from you! If you have any questions,
-    comments or feedback, please don't hesitate to get in touch
-    with us using the contact details below.
-  </p>
-  <div className="flex flex-col items-center mb-8">
-    <div className="flex items-center mb-3">
-      <div className="mr-4">
-        <FontAwesomeIcon icon={faEnvelope} className="text-gray-600 text-lg" />
-      </div>
-      <div>
-        <a href="mailto:epictours@gmail.com" className="text-gray-700 hover:text-black font-medium">
-          epictours@gmail.com
-        </a>
-      </div>
-    </div>
-    <div className="flex items-center mb-3">
-      <div className="mr-4">
-        <FontAwesomeIcon icon={faPhone} className="text-gray-600 text-lg" />
-      </div>
-      <div>
-        <a href="tel:+07239887898" className="text-gray-700 hover:text-black font-medium">
-          0723 988 7898
-        </a>
-      </div>
-    </div>
-    <div className="flex items-center mb-3">
-      <div className="mr-4">
-        <FontAwesomeIcon icon={faMapMarkerAlt} className="text-gray-600 text-lg" />
-      </div>
-      <div>
-        <span className="text-gray-700 font-medium">
-          Epic Building, Suite 301, Kenyatta Avenue
-        </span>
-      </div>
-    </div>
-  </div>
-  <div className="flex justify-center">
-    <div className="inline-flex">
-      <a href="https://www.facebook.com/" className="text-gray-700 hover:text-black mr-4">
-        <FontAwesomeIcon icon={faFacebook} className="text-2xl" />
-      </a>
-      <a href="https://twitter.com/" className="text-gray-700 hover:text-black mr-4">
-        <FontAwesomeIcon icon={faTwitter} className="text-2xl" />
-      </a>
-      <a href="https://www.instagram.com/" className="text-gray-700 hover:text-black mr-4">
-        <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
-      </a>
-      <a href="https://ke.linkedin.com" className="text-gray-700 hover:text-black">
-        <FontAwesomeIcon icon={faLinkedin} className="text-2xl" />
-      </a>
-    </div>
-  </div>
-</div>
-
+              <div className="text-black mb-10 bg-white p-10">
+                <h2 className="text-2xl font-bold mb-10 text-black mt-20">
+                  Contact Us
+                </h2>
+                <p className="mb-5">
+                  We would love to hear from you! If you have any questions,
+                  comments or feedback, please don't hesitate to get in touch
+                  with us using the contact details below.
+                </p>
+                <div className="flex mb-10">
+                  <div className="mr-2 bg-white">
+                    <b>Email:</b>
+                  </div>
+                  <div className="mr-2">epictours@gmail.com</div>
+                </div>
+                <div className="flex mb-10">
+                  <div className="mr-2">
+                    <b>Phone:</b>
+                  </div>
+                  <div className="mr-2">07239887898</div>
+                </div>
+                <div className="flex mb-10">
+                  <div className="mr-2">
+                    <b>Address:</b>
+                  </div>
+                  <div className="mr-2">
+                    Epic Building, Suite 301, Kenyatta Avenue
+                  </div>
+                </div>
+                <div className="social-media-icons mb-20 ">
+                  <a href="https://www.facebook.com/">
+                    <FontAwesomeIcon icon={faFacebook} />
+                  </a>
+                  <a href="https://twitter.com/">
+                    <FontAwesomeIcon icon={faTwitter} />
+                  </a>
+                  <a href="https://www.instagram.com/">
+                    <FontAwesomeIcon icon={faInstagram} />
+                  </a>
+                  <a href="https://ke.linkedin.com">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                  </a>
+                </div>
+              </div>
+            </div>
             <div className="col-md-6 bg-gray-200">
               <form
                 className="max-w-6xl mx-auto"
@@ -167,7 +153,6 @@ const ContactUs = () => {
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
